1b441f46da7bb00753d2c40364255156fa6d01ef
rockpell <rockpell.cleaning@gmail.com>
2021-07-31 15:07:27 +0900

    Add: 랜덤 상품 조회 기능

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import "./App.css";
 import React from "react";
-import { getProductData } from "./utils";
+import { getProductData, setLocalStorage, getRandom } from "./utils";
+import { recentProduct } from "./constant";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       productData: [],
+      recentProduct: [],
+      nowProduct: {},
     };
+
+    this.randomSearch = this.randomSearch.bind(this);
   }
 
   async componentDidMount() {
@@ -15,8 +21,97 @@ class App extends React.Component {
     this.setState({ productData: jsonData });
   }
 
+  addRecentProduct(product) {
+    const prevProduct = this.findRecentProduct(product);
+
+    if (prevProduct) {
+      const subProductList = this.state.recentProduct.filter(
+        (item) => item.title !== product.title
+      );
+      const newProduct = { ...prevProduct };
+
+      this.setState({ recentProduct: [...subProductList, newProduct] });
+      setLocalStorage(recentProduct, [...subProductList, newProduct]);
+    } else {
+      const newProduct = { ...product };
+
+      this.setState({
+        recentProduct: [...this.state.recentProduct, newProduct],
+      });
+      setLocalStorage(recentProduct, [...this.state.recentProduct, newProduct]);
+    }
+  }
+
+  findRecentProduct(product) {
+    const prevProduct = this.state.recentProduct.find(
+      (item) => item.title === product.title
+    );
+
+    return prevProduct;
+  }
+
+  getRandomProduct(allProductList, excludeProductList) {
+    const targetProductList = allProductList.filter(
+      (item) =>
+        !excludeProductList.find(
+          (excludeItem) => excludeItem.title === item.title
+        )
+    );
+
+    const randValue = getRandom(0, targetProductList.length);
+    return targetProductList[randValue];
+  }
+
+  randomSearch() {
+    const product = this.getRandomProduct(this.state.productData, [
+      this.state.nowProduct,
+    ]);
+
+    product.date = new Date();
+    product.isIgnore = false;
+    this.setState({ nowProduct: product });
+    this.addRecentProduct(product);
+  }
+
+  getProductComponent(product, isRecent = false) {
+    const { title, brand, price, date } = product;
+
+    return (
+      <div key={title} className="product-div">
+        <span className="block-span">title: {title}</span>
+        <span className="block-span">brand: {brand}</span>
+        <span className="block-span">price: {price}</span>
+        {isRecent && (
+          <span className="block-span">date: {date.toString()}</span>
+        )}
+      </div>
+    );
+  }
+
+  getRecentProductCompoent() {
+    const recentProduct = this.state.recentProduct;
+
+    return recentProduct.map((item) => this.getProductComponent(item, true));
+  }
+
   render() {
-    return <div>hello!</div>;
+    return (
+      <>
+        <div>
+          <div>
+            <button onClick={this.randomSearch}>랜덤 조회</button>
+          </div>
+          <div>
+            현재 상품
+            {this.getProductComponent(this.state.nowProduct)}
+          </div>
+        </div>
+        <div>
+          최근 이력
+          {this.getRecentProductCompoent()}
+        </div>
+      </>
+    );
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,32 @@ export const getProductData = async () => {
   try {
     const data = await fetch("product_data.json");
     const jsonData = await data.json();
-    return jsonData;
+    return jsonData.filter((_, index) => index < 10);
   } catch (e) {
     console.log("error: json file load fail");
   }
+};
+
+export const getLocalStorage = (key) => {
+  const data = localStorage.getItem(key);
+
+  return JSON.parse(data);
+};
+
+export const setLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const deleteLocalStorage = (key) => {
+  localStorage.removeItem(key);
+};
+
+export const clearLocalStorage = () => {
+  localStorage.clear();
+};
+
+export const getRandom = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 };
